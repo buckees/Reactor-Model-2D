f2cc4beda78eb7ce82dc11feb0304d4ab3817529
buckees <67809187+buckees@users.noreply.github.com>
2020-11-01 22:16:03 -0800

    Create Rectangle()
    Add doc for Interval

--- Geometry.py
+++ Geometry.py
@@ -18,31 +18,70 @@ class Shape:
 
 
 class Interval(Shape):
-    """
-    Interval is a 1D basic shape.
+    """Interval is a 1D basic shape."""
     
-    begin: unit in m, var
-    end: unit in m, var
-    axis: ???
-    """
     def __init__(self, begin, end, axis=0):
+        """
+        Init the Interval.
+        
+        begin: unit in m, var
+        end: unit in m, var
+        axis: ???
+        """
         self.begin = begin
         self.end = end
         self.axis = axis
 
     def __str__(self):
+        """Print Interval info."""
         res = 'Interval:'
-        res += f'\nbegin = {self.begin}'
-        res += f'\nend = {self.end}'
+        res += f'\nbegin = {self.begin} m'
+        res += f'\nend = {self.end} m'
         res += f'\naxis = {self.axis}'
         return res
 
-    def __contains__(self, location):
-        return self.begin < location[self.axis] < self.end
-
+    def __contains__(self, posn):
+        """
+        Determind if a position is inside the Interval.
+        
+        posn: unit in m, var or 1D array, position as input
+        boundaries are not consindered as "Inside"
+        """
+        return self.begin < posn[self.axis] < self.end
+    
+    def boundary(self):
+        """Return the boundary."""
+        pass
 
 class Rectangle(Shape):
-    pass
+    """Rectangle is a 2D basic shape."""
+    
+    def __init__(self, bottom_left, up_right):
+        """
+        Init the Rectangle.
+        
+        bottom_left: unit in m, 2-item list, [x, y]
+        up_right: unit in m, 2-item list, [x, y]
+        """
+        self.bl = bottom_left
+        self.ur = up_right
+
+    def __str__(self):
+        """Print Rectangle info."""
+        res = 'Rectangle:'
+        res += f'\nbottom left = {self.bl} m'
+        res += f'\nup right = {self.ur} m'
+        return res
+
+    def __contains__(self, posn):
+        """
+        Determind if a position is inside the Interval.
+        
+        posn: unit in m, 2-item list or 2D array, position as input
+        boundaries are not consindered as "Inside"
+        """
+        return ((self.bl[0] < posn[0] < self.ur[0]) and 
+                (self.bl[1] < posn[1] < self.ur[1]))
 
 
 class Geometry:
@@ -63,16 +102,16 @@ class Geometry:
     def add_shape(self, shape):  # is this shape a class?
         self.sequence.append(shape)
 
-    def get_label(self, location):  # overlap?
+    def get_label(self, posn):  # overlap?
         label = None
         for shape in self.sequence:
-            if location in shape:
+            if posn in shape:
                 label = shape.label
         return label
 
-    def find_in_label(self, location, label):  # ??? don't understand
+    def find_in_label(self, posn, label):  # ??? don't understand
         res = False
         for shape in self.sequence:
             if shape.label == label:
-                res = res or (location in shape)
+                res = res or (posn in shape)
         return res
