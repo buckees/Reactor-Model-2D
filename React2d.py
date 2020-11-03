a779b29f33a78e3c7d77d41fe229d6298aa38365
buckees <67809187+buckees@users.noreply.github.com>
2020-11-02 23:54:00 -0800

    Create React2d.py

---
+++ React2d.py
@@ -0,0 +1,25 @@
+"""
+2D Plasma Transport Module.
+
+Transp_1d contains:
+    Diffusion only
+    Ambipolar
+    Drfit-Diffusion
+    Momentum Solver
+
+    Continuity Eq. dn/dt = -dF/dx * dt + S * det
+    Input: depends on transport mode
+    Output: dF/dx for continuity equation.
+"""
+
+import numpy as np
+
+class React_2d(object):
+    """Define the base tranport module/object."""
+    
+    def __init__(self, pla):
+        """Import geometry information."""
+        _x = pla.geom.x
+        self.se = np.zeros_like(_x)  # initial eon flux
+        self.si = np.zeros_like(_x)  # initial ion flux
+        
