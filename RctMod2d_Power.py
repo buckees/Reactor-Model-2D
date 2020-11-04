262243be3d8b3b60798e7e9776a47984eb153c15
buckees <67809187+buckees@users.noreply.github.com>
2020-11-04 14:18:24 -0800

    Update doc

--- RctMod2d_Power.py
+++ RctMod2d_Power.py
@@ -12,26 +12,26 @@ import numpy as np
 import matplotlib.pyplot as plt
 from copy import deepcopy
 
-class Power_1d(object):
-    """Define the eon energy module/object."""
+class Power2d(object):
+    """Define the power module/object."""
     
     def __init__(self, pla):
         """Import Plasma1d information."""
-        nx = pla.geom.nx
-        self.input = np.zeros(nx)  # initial eon flux
+        _nx = pla.mesh.nx
+        self.input = np.zeros(_nx)  # initial eon flux
         
         
     def __str__(self):
         """Print eon energy module."""
-        return f'label = {self.qdfluxe}'
+        return f'power input = {self.input}'
     
     def calc_pwr_in(self, pla):
         """
-        Calc thermal conduction coefficient.
+        Calc input power.
 
-        pla: Plasma_1d object
+        pla: Plasma2d object
              calc uses pla.Te,i and pla.coll_em
-        heat_cond_e: W/m/K, heat conductivity for eon
+        input: W, (nz, nx) matrix, power input
         """
         # calc thermal conductivity for eon
         self.input = np.ones_like(pla.ne)*1.0
