a6a73bd36c947eaac480964235d771ccafe4d339
buckees <67809187+buckees@users.noreply.github.com>
2020-11-03 23:44:23 -0800

    Rename .geom to .mesh

--- RctMod2d_Transp.py
+++ RctMod2d_Transp.py
@@ -28,7 +28,7 @@ class Transp_2d(object):
     
     def __init__(self, pla):
         """Import geometry information."""
-        _x = pla.geom.x
+        _x = pla.mesh.x
         self.fluxe = np.zeros_like(_x)  # initial eon flux
         self.fluxi = np.zeros_like(_x)  # initial ion flux
         self.dfluxe = np.zeros_like(_x)  # initial eon flux
@@ -56,9 +56,9 @@ class Transp_2d(object):
         Plot transp coeff.
         
         pla: Plasma_1d object
-            use pla.geom.x for plot
+            use pla.mesh.x for plot
         """
-        _x, _z = pla.geom.x, pla.geom.z
+        _x, _z = pla.mesh.x, pla.mesh.z
         fig, axes = plt.subplots(1, 2, figsize=figsize, dpi=dpi,
                                  constrained_layout=True)
         # plot densities
@@ -81,9 +81,9 @@ class Transp_2d(object):
         Plot flux and dflux.
         
         pla: Plasma_1d object
-            use pla.geom.x for plot
+            use pla.mesh.x for plot
         """
-        _x, _z = pla.geom.x, pla.geom.z
+        _x, _z = pla.mesh.x, pla.mesh.z
         fig, axes = plt.subplots(1, 2, figsize=figsize, dpi=dpi,
                                  constrained_layout=True)
         # plot densities
@@ -114,11 +114,11 @@ class Diff_2d(Transp_2d):
         # Calc transp coeff first
         self.calc_transp_coeff(pla)
         # Calc flux
-        self.fluxex, self.fluxez = -self.De * pla.geom.cnt_diff(pla.ne)
-        self.fluxix, self.fluxiz = -self.Di * pla.geom.cnt_diff(pla.ni)
+        self.fluxex, self.fluxez = -self.De * pla.mesh.cnt_diff(pla.ne)
+        self.fluxix, self.fluxiz = -self.Di * pla.mesh.cnt_diff(pla.ni)
         # Calc dflux
-        self.dfluxe = -self.De * pla.geom.cnt_diff_2nd(pla.ne)
-        self.dfluxi = -self.Di * pla.geom.cnt_diff_2nd(pla.ni)
+        self.dfluxe = -self.De * pla.mesh.cnt_diff_2nd(pla.ne)
+        self.dfluxi = -self.Di * pla.mesh.cnt_diff_2nd(pla.ni)
 
     
 class Ambi_2d(Transp_2d):
@@ -154,17 +154,17 @@ class Ambi_2d(Transp_2d):
         self.calc_transp_coeff(pla)
         # Calc ambi coeff
         self.Da = self.Di*(1.0 + np.divide(pla.Te, pla.Ti))
-        _dnix, _dniz = pla.geom.cnt_diff(pla.ni)
+        _dnix, _dniz = pla.mesh.cnt_diff(pla.ni)
         self.Eax = np.divide(self.Di - self.De, self.Mui + self.Mue)
         self.Eaz = deepcopy(self.Eax)
         self.Eax *= np.divide(_dnix, pla.nix)
         self.Eaz *= np.divide(_dniz, pla.niz)
         # # Calc flux
-        self.fluxex, self.fluxez = -self.Da * pla.geom.cnt_diff(pla.ne)
-        self.fluxix, self.fluxiz = -self.Da * pla.geom.cnt_diff(pla.ni)
+        self.fluxex, self.fluxez = -self.Da * pla.mesh.cnt_diff(pla.ne)
+        self.fluxix, self.fluxiz = -self.Da * pla.mesh.cnt_diff(pla.ni)
         # Calc dflux
-        self.dfluxe = -self.Da * pla.geom.cnt_diff_2nd(pla.ne)
-        self.dfluxi = -self.Da * pla.geom.cnt_diff_2nd(pla.ni)
+        self.dfluxe = -self.Da * pla.mesh.cnt_diff_2nd(pla.ne)
+        self.dfluxi = -self.Da * pla.mesh.cnt_diff_2nd(pla.ni)
         # self.bndy_ambi()
 
 
