46a91f7dcd19952fd4cc8eb0fe1bd4c44f07c773
buckees <67809187+buckees@users.noreply.github.com>
2020-11-04 15:41:20 -0800

    Add get_pwr() and Make calc_Te() as 1-stop calc

--- RctMod2d_Eergy.py
+++ RctMod2d_Eergy.py
@@ -21,6 +21,7 @@ class Eergy2d(object):
     def __init__(self, pla):
         """Import Plasma1d information."""
         self.Te = deepcopy(pla.Te)
+        self.pwr = np.zeros_like(pla.Te)
         # eon energy = 3/2 * ne * kTe
         self.ergy_e = 1.5*KB_EV*np.multiply(pla.ne, pla.Te)
         
@@ -29,25 +30,33 @@ class Eergy2d(object):
         """Print eon energy module."""
         return f'energy flux = {self.qdfluxe}'
     
-    def calc_th_cond_coeff(self, pla):
+    def get_pwr(self, pwr):
+        """
+        Get input power from Power2d().
+        
+        pwr: Power2d() object.
+        """
+        self.pwr = deepcopy(pwr.input)
+    
+    def _calc_th_cond_coeff(self, pla):
         """
         Calc thermal conduction coefficient.
 
-        pla: Plasma2d object
+        pla: Plasma2d() object.
         heat_cond_e: W/m/K, (nz, nx) matrix, heat conductivity for eon
         th_cond_e depend only on pla.
         """
         # calc thermal conductivity for eon
         self.th_cond_e = np.ones_like(pla.ne)*1e-3
     
-    def calc_th_flux(self, pla, txp):
+    def _calc_th_flux(self, pla, txp):
         """
         Calc eon thermal flux, Qe.
         
         Qe = 5/2kTe * fluxe - ke * dTe/dx
         dQe = 5/2kTe * dfluxe - ke * d2Te/dx2
-        pla: Plasma2d object
-        txp: Transp2d object
+        pla: Plasma2d() object
+        txp: Transp2d() object
         """
         # calc convection term
         self.Qex = 2.5*KB_EV*np.multiply(self.Te, txp.fluxex)
@@ -60,58 +69,16 @@ class Eergy2d(object):
         self.Qez -= np.multiply(self.th_cond_e, self.dTez)
         self.dQe -= np.multiply(self.th_cond_e, self.d2Te)
         
-    def calc_Te(self, delt, pla, pwr):
-        """Calc Te."""
-        self.ergy_e += (-self.dQe + pwr.input)*delt
+    def calc_Te(self, delt, pla, txp):
+        """
+        Calc Te.
+        
+        pla: Plasma2d() object.
+        txp: Transp2d() object.
+        """
+        self._cal_th_cond_coeff(pla)
+        self._calc_th_flux(pla, txp)
+        self.ergy_e += (-self.dQe + self.pwr)*delt
         self.Te = np.divide(self.ergy_e, pla.ne)/1.5/KB_EV
         
         
-if __name__ == '__main__':
-    """Test Eergy_1d."""
-    from Mesh import Mesh_1d
-    from Plasma1d import Plasma_1d
-    from Transp1d import Ambi_1d
-    from React1d import React_1d
-    from Power1d import Power_1d
-    mesh1d = Mesh_1d('Plasma_1d', 10e-2, nx=51)
-    print(mesh1d)
-    pla1d = Plasma_1d(mesh1d)
-    pla1d.init_plasma()
-    pla1d.plot_plasma()
-    # calc the transport 
-    txp1d = Ambi_1d(pla1d)
-    txp1d.calc_transp_coeff(pla1d)
-    txp1d.plot_transp_coeff(pla1d)
-    # calc source term
-    src1d = React_1d(pla1d)
-    #
-    ne_ave, ni_ave = [], []
-    time = []
-    dt = 1e-6
-    niter = 3000
-    for itn in range(niter):
-        txp1d.calc_ambi(pla1d)
-        pla1d.den_evolve(dt, txp1d, src1d)
-        pla1d.bndy_plasma()
-        pla1d.limit_plasma()
-        # ne_ave.append(np.mean(pla1d.ne))
-        # ni_ave.append(np.mean(pla1d.ni))
-        # time.append(dt*(niter+1))
-        # if not (itn+1) % (niter/10):
-        #     txp1d.plot_flux(pla1d)
-        #     pla1d.plot_plasma()
-    # Test Eergy_1d
-    dt = 3e-7
-    niter = 100000
-    een1d = Eergy_1d(pla1d)
-    pwr1d = Power_1d(pla1d)
-    pwr1d.calc_pwr_in(pla1d)
-    een1d.plot_Te(pla1d)
-    for itn in range(niter):
-        een1d.calc_th_cond_coeff(pla1d)
-        een1d.calc_th_flux(pla1d, txp1d)
-        een1d.calc_Te(dt, pla1d, pwr1d)
-        een1d.bndy_Te()
-        if not (itn+1) % (niter/10):
-            een1d.plot_Te(pla1d)
-            
