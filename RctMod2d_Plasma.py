0f0e5efbf18f8ac8050b172ecd489139b9be0607
buckees <67809187+buckees@users.noreply.github.com>
2020-11-03 23:34:40 -0800

    Rename .geom to .mesh

--- RctMod2d_Plasma.py
+++ RctMod2d_Plasma.py
@@ -48,7 +48,7 @@ class Plasma2d(object):
                             1e9 at 1000 mTorr
         Mi: kg, ion mass
         """
-        _x = self.geom.x
+        _x = self.mesh.x
         self.ne = np.ones_like(_x)*ne  # init uniform ne on 1d mesh
         self.ni = np.ones_like(_x)*ne  # init ni to neutralize ne
         self.nn = np.ones_like(_x)*(press*3.3e19)  # init neutral density
@@ -64,7 +64,7 @@ class Plasma2d(object):
 
     def set_bc(self):
         """Impose b.c. on the plasma."""
-        for _idx in self.geom.bndy_list:
+        for _idx in self.mesh.bndy_list:
             self.ne[_idx] = 1e11
             self.ni[_idx] = 1e11
             self.nn[_idx] = 1e11
@@ -97,7 +97,7 @@ class Plasma2d(object):
         density, flux, temperature
         imode: str, var, ['Contour', 'Scatter']
         """
-        _x, _z = self.geom.x, self.geom.z
+        _x, _z = self.mesh.x, self.mesh.z
         if ihoriz:
             fig, axes = plt.subplots(1, 2, figsize=figsize, dpi=dpi,
                                      constrained_layout=True)
@@ -134,14 +134,14 @@ class Plasma2d(object):
 
     def init_pot(self, phi=0.0):
         """Initiate potential attributes."""
-        nx = self.geom.nx
+        nx = self.mesh.nx
         self.pot = np.ones(nx)*phi  # initial uniform potential
         self.ef = np.zeros_like(self.pot)  # initial uniform E-field
         self.ef_ambi = np.zeros_like(self.pot)  # initial ambipolar E-field
 
     def plot_pot(self):
         """Plot potential, E-field."""
-        x = self.geom.x
+        x = self.mesh.x
         fig, axes = plt.subplots(1, 2, figsize=(8, 4),
                                  constrained_layout=True)
         # plot potential
@@ -171,21 +171,40 @@ class Plasma2d(object):
 if __name__ == '__main__':
     """Test Plasma_1d."""
     from RctMod2d_Mesh import Mesh2d
-    from RctMod2d_Transp2d import Diff_2d, Ambi_2d
-    from RctMod2d_React2d import React_2d
-    domain=(2.0, 4.0)
-    mesh2d = Mesh(bl=(-1.0, 0.0), domain=domain, ngrid=(21, 41))
-    mesh2d.find_bndy()
+    from RctMod2d_Transp import Diff_2d, Ambi_2d
+    from RctMod2d_React import React_2d
+    from RctMod2d_Geom import Geom2d, Domain, Rectangle
+    # build the geometry
+    geom2d = Geom2d(name='2D Plasma', is_cyl=False)
+    domain = Domain((-1.0, 0.0), (2.0, 4.0))
+    geom2d.add_domain(domain)
+    top = Rectangle('Metal', (-1.0, 3.5), (1.0, 4.0))
+    geom2d.add_shape(top)
+    bott = Rectangle('Metal', (-0.5, 0.0), (0.5, 0.2))
+    geom2d.add_shape(bott)
+    left = Rectangle('Metal', (-1.0, 0.0), (-0.9, 4.0))
+    geom2d.add_shape(left)
+    right = Rectangle('Metal', (0.9, 0.0), (1.0, 4.0))
+    geom2d.add_shape(right)
+    quartz = Rectangle('Quartz', (-0.9, 3.3), (0.9, 3.5))
+    geom2d.add_shape(quartz)
+    geom2d.plot(fname='geom2d.png')
+    print(geom2d)
+    # generate mesh to imported geometry
+    mesh2d = Mesh2d()
+    mesh2d.import_geom(geom2d)
+    mesh2d.generate_mesh(ngrid=(21, 41))
     mesh2d.plot()
+
     
     pla2d = Plasma2d(mesh2d)
     pla2d.init_plasma()
 
-    if domain[0] > domain[1]:
-        figsize = tuple([domain[0], domain[1]*2])
+    if domain.domain[0] > domain.domain[1]:
+        figsize = tuple([domain.domain[0], domain.domain[1]*2])
         ihoriz = 0
     else:
-        figsize = tuple([domain[0]*2, domain[1]])
+        figsize = tuple([domain.domain[0]*2, domain.domain[1]])
         ihoriz = 1
     pla2d.plot_plasma(figsize=figsize, ihoriz=ihoriz)
     
