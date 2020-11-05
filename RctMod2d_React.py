69edc2bcb92f1c904def5dc940687eff3ea2543f
buckees <67809187+buckees@users.noreply.github.com>
2020-11-04 19:48:33 -0800

    Add plot_src()

--- RctMod2d_React.py
+++ RctMod2d_React.py
@@ -13,6 +13,11 @@ Transp_1d contains:
 """
 
 import numpy as np
+from copy import copy, deepcopy
+import matplotlib.pyplot as plt
+import matplotlib.cm as cm
+colMap = copy(cm.get_cmap("jet"))
+colMap.set_under(color='white')
 
 class React2d(object):
     """Define the base tranport module/object."""
@@ -29,3 +34,44 @@ class React2d(object):
         self.se = np.multiply(self.se, self.ke)
         self.si = np.multiply(pla.ne, pla.nn)
         self.si = np.multiply(self.si, self.ke)
+        
+    def plot_src(self, pla, figsize=(8, 8), ihoriz=1, 
+                    dpi=300, fname='Power.png', imode='Contour'):
+        """
+        Plot power vs. position.
+            
+        var include input power and total power.
+        figsize: a.u., (2, ) tuple, size of fig
+        ihoriz: a.u., var, 0 or 1, set the layout of fig horizontal or not
+        dpi: a.u., dots per inch
+        fname: str, var, name of png file to save
+        imode: str, var, ['Contour', 'Scatter']
+        """
+        _x, _z = pla.mesh.x, pla.mesh.z
+        if ihoriz:
+            fig, axes = plt.subplots(1, 2, figsize=figsize, dpi=dpi,
+                                     constrained_layout=True)
+        else:
+            fig, axes = plt.subplots(2, 1, figsize=figsize, dpi=dpi,
+                                     constrained_layout=True)
+        
+        # plot densities
+        if imode == 'Contour':
+            for _ax, _den, _title in zip(axes, (self.Se, self.Si), 
+                                ('Eon Source', 'Ion Source')):
+                _cs = _ax.contourf(_x, _z, _den, cmap=colMap)
+                _ax.set_title(_title)
+                fig.colorbar(_cs, ax=_ax, shrink=0.9)
+            
+        elif imode == 'Scatter':
+            for _ax, _den, _title in zip(axes, (self.Se, self.Si), 
+                                ('Eon Source', 'Ion Source')):
+                _ax.scatter(_x, _z, c=_den, s=5, cmap=colMap)
+                _ax.set_title(_title)
+            
+        for ax in axes:
+            ax.set_xlabel('Position (m)')
+            ax.set_ylabel('Height (m)')
+            ax.set_aspect('equal')
+        fig.savefig(fname, dpi=dpi)
+        plt.close()
