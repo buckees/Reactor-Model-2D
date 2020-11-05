39897a179f76a8a989ee2d350fad8b22fe6443ba
buckees <67809187+buckees@users.noreply.github.com>
2020-11-04 19:42:10 -0800

    Add calc_src()

--- RctMod2d_React.py
+++ RctMod2d_React.py
@@ -21,4 +21,11 @@ class React2d(object):
         """Import geometry information."""
         self.se = np.zeros_like(pla.ne)  # initial eon flux
         self.si = np.zeros_like(pla.ne)  # initial ion flux
-        
+    
+    def calc_src(self, pla, ke=1.0):
+        """Calc src due to ionization."""
+        self.ke = ke * np.sqrt(pla.Te)
+        self.se = np.multiply(pla.ne, pla.nn)
+        self.se = np.multiply(self.se, self.ke)
+        self.si = np.multiply(pla.ne, pla.nn)
+        self.si = np.multiply(self.si, self.ke)
