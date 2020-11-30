dd8a5783d8c2cb50fa73de1ee10881dd270e0652
buckees <67809187+buckees@users.noreply.github.com>
2020-11-30 10:25:14 -0800

    Add plot_dQe()

--- RctMod2d_Eergy.py
+++ RctMod2d_Eergy.py
@@ -11,8 +11,12 @@ Eergy_2d contains:
 from Constants import KB_EV, EON_MASS, UNIT_CHARGE
 
 import numpy as np
+from copy import copy, deepcopy
 import matplotlib.pyplot as plt
-from copy import deepcopy
+import matplotlib.cm as cm
+colMap = copy(cm.get_cmap("jet"))
+colMap.set_under(color='white')
+
 
 
 class Eergy2d(object):
@@ -36,7 +40,7 @@ class Eergy2d(object):
         
         pwr: Power2d() object.
         """
-        self.pwr = deepcopy(pwr.input)
+        self.pwr = pwr.input
     
     def _calc_th_cond_coeff(self, pla):
         """
@@ -99,4 +103,44 @@ class Eergy2d(object):
         self._set_bc(pla)
         self._set_nonPlasma(pla)
         self._limit_Te()
+
+    def plot_dQe(self, pla, figsize=(8, 8), ihoriz=1, 
+                    dpi=300, fname='dQe.png', imode='Contour'):
+        """
+        Plot power vs. position.
+            
+        var include input power and total power.
+        figsize: a.u., (2, ) tuple, size of fig
+        ihoriz: a.u., var, 0 or 1, set the layout of fig horizontal or not
+        dpi: a.u., dots per inch
+        fname: str, var, name of png file to save
+        imode: str, var, ['Contour', 'Scatter']
+        """
+        _x, _z = pla.mesh.x, pla.mesh.z
+        if ihoriz:
+            fig, axes = plt.subplots(1, 2, figsize=figsize, dpi=dpi,
+                                     constrained_layout=True)
+        else:
+            fig, axes = plt.subplots(2, 1, figsize=figsize, dpi=dpi,
+                                     constrained_layout=True)
         
+        # plot densities
+        if imode == 'Contour':
+            for _ax, _den, _title in zip(axes, (self.dQe, self.pwr), 
+                                ('Power Loss', 'Power to Eon')):
+                _cs = _ax.contourf(_x, _z, _den, cmap=colMap)
+                _ax.set_title(_title)
+                fig.colorbar(_cs, ax=_ax, shrink=0.9)
+            
+        elif imode == 'Scatter':
+            for _ax, _den, _title in zip(axes, (self.Te, self.Ti), 
+                                ('E Temperature', 'Ion Temperature')):
+                _ax.scatter(_x, _z, c=_den, s=5, cmap=colMap)
+                _ax.set_title(_title)
+            
+        for ax in axes:
+            ax.set_xlabel('Position (m)')
+            ax.set_ylabel('Height (m)')
+            ax.set_aspect('equal')
+        fig.savefig(fname, dpi=dpi)
+        plt.close()        
